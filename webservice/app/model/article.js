960e22218c1b849a0039d4ff9709f0f31f355267
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-08-13 10:56:50 +0800

    更新index

--- webservice/app/model/article.js
+++ webservice/app/model/article.js
@@ -7,7 +7,7 @@ module.exports = app => {
     authorId: { type: String }, // 作者用户ID
     authorName: { type: String, default:'未署名'}, // 注意作者的名字并不来自于用户信息，像微信公众号一样是发布文章时指定的
     title: { type: String ,default:'无标题'},
-    content: { type: String, default:'' },
+    content: { type: String, default:'', index:true },
     abstract: { type: String,default:'' },
     refLink: { type:String, default: ''}, // 外链
     coverImg: { type: String, default:'' }, // 封面图片
@@ -19,8 +19,6 @@ module.exports = app => {
     limited:{type:Boolean, default:false}, // 权限文章预留字段
     viewCount:{type:Number, default:0} // 阅读量
   });
-
-  ArticleSchema.path('content').index({text : true});
 
   return mongoose.model('Article', ArticleSchema);
 }
